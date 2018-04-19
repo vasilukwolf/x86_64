ce3f0d925f812ed5596c7577326408a7831369d6
Vasiluk Dmitry <vasilukwolf@gmail.com>
2018-04-18 20:56:54 -0400

    Init&Hello Universe x64

---
+++ helloUniverse/hello.asm
@@ -0,0 +1,17 @@
+section .date
+	text db "Hello Universe", 10
+	
+
+section .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text
+	mov rdx, 14
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
